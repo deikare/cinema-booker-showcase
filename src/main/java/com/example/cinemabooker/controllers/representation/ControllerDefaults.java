68ee3b7f24ac8e662dcd25f5c396106d4e9f03de
deikare <bbgolawski@gmail.com>
2023-04-23 17:44:53 +0200

    Improve comments

--- src/main/java/com/example/cinemabooker/controllers/representation/ControllerDefaults.java
+++ src/main/java/com/example/cinemabooker/controllers/representation/ControllerDefaults.java
@@ -6,7 +6,7 @@ public final class ControllerDefaults {
     }
 
     public static final int PAGE_NUMBER = 0;
-    public static final String PAGE_NUMBER_AS_STRING = "0";
+    public static final String PAGE_NUMBER_AS_STRING = "0"; //Spring requires const value instead of String.valueOf(...)
     public static final int PAGE_SIZE = 5;
     public static final String PAGE_SIZE_AS_STRING = "5";
 }
