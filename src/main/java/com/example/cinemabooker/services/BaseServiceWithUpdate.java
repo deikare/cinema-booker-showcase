e2e8daf893fb991fb511e1f44d00338fc0adac3f
deikare <bbgolawski@gmail.com>
2023-04-24 01:14:04 +0200

    Rewrite patch implementation to match create method

--- src/main/java/com/example/cinemabooker/services/BaseServiceWithUpdate.java
+++ src/main/java/com/example/cinemabooker/services/BaseServiceWithUpdate.java
@@ -1,5 +1,6 @@
 package com.example.cinemabooker.services;
 
+import com.example.cinemabooker.services.exceptions.IdAlreadyDefinedException;
 import com.example.cinemabooker.services.interfaces.EntityUpdateInterface;
 import org.slf4j.Logger;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,18 +10,17 @@ public class BaseServiceWithUpdate<T extends EntityUpdateInterface<T>> extends B
         super(repository, logger);
     }
 
-    void update(String id, T newEntity) {
-        T entity = repository.findById(id)
+    void update(String id, T newEntity) throws IdAlreadyDefinedException {
+        repository.findById(id)
                 .map(t -> { //update if exists
                     t.update(newEntity);
                     logger.info("Entity updated, current state: " + t);
-                    return t;
+                    return save(t);
                 })
                 .orElseGet(() -> { //create otherwise
-                    newEntity.setId(id);
+                    create(newEntity);
                     logger.info("Entity not found, new entity created");
                     return newEntity;
                 });
-        save(entity);
     }
 }
