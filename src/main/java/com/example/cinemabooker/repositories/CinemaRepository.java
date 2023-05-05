6cd5c81c3533a077aba6d9171ec431c6a9c60e4d
deikare <bbgolawski@gmail.com>
2023-05-05 03:45:54 +0200

    Remove unnecessary projections:

--- src/main/java/com/example/cinemabooker/repositories/CinemaRepository.java
+++ src/main/java/com/example/cinemabooker/repositories/CinemaRepository.java
@@ -1,12 +1,9 @@
 package com.example.cinemabooker.repositories;
 
 import com.example.cinemabooker.model.Cinema;
-import com.example.cinemabooker.model.projections.TestProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RepositoryRestResource(excerptProjection = TestProjection.class)
 public interface CinemaRepository extends JpaRepository<Cinema, String> {
 }
--- src/main/java/com/example/cinemabooker/repositories/ScreeningRepository.java
+++ src/main/java/com/example/cinemabooker/repositories/ScreeningRepository.java
@@ -1,19 +1,16 @@
 package com.example.cinemabooker.repositories;
 
 import com.example.cinemabooker.model.Screening;
-import com.example.cinemabooker.model.projections.ScreeningWithRoomNumberProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
 import java.util.Optional;
 
 @Repository
-@RepositoryRestResource(excerptProjection = ScreeningWithRoomNumberProjection.class)
 public interface ScreeningRepository extends JpaRepository<Screening, String> {
     Page<Screening> findAllByScreeningTimeBetween(Instant start, Instant end, Pageable pageable);
     Page<Screening> findAllByMovieId(String movieId, Pageable pageable);
