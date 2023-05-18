6e1a9b591a9c6dbdb0ef13401066a851bc8e917d
deikare <bbgolawski@gmail.com>
2023-05-18 17:46:32 +0200

    Remove unused queries

--- src/main/java/com/example/cinemabooker/repositories/ScreeningRepository.java
+++ src/main/java/com/example/cinemabooker/repositories/ScreeningRepository.java
@@ -17,6 +17,4 @@ public interface ScreeningRepository extends JpaRepository<Screening, String> {
     Page<Screening> findAllByMovieId(String movieId, Pageable pageable);
     @Query("SELECT s FROM Screening s JOIN FETCH s.seatsRows WHERE s.entityId = (:id)")
     Optional<Screening> findByIdAndFetchSeatsRows(String id);
-    @Query("SELECT s FROM Screening s LEFT JOIN FETCH s.seatsRows sr LEFT JOIN FETCH sr.seats se WHERE s.entityId = :id ORDER BY sr.position ASC, se.position ASC")
-    Optional<Screening> findByIdWithSortedSeatRowsAndSeats(@Param("id") String id);
 }
